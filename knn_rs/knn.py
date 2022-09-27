e3fbf04d987ec32c5875b66d126b7cd07a37fc65
wenig <phillip.wenig@hpi.de>
2022-09-27 11:08:19 +0200

    feat: add python knn

---
+++ knn_rs/knn.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Optional
+from collections import Counter
+
+import numpy as np
+
+from .knn_rs import knn_algorithm
+
+
+class KNN(ABC):
+    @abstractmethod
+    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        ...
+
+
+class PythonKNN(KNN):
+    def __init__(self, k: int = 3):
+        self.k = k
+        self._X: Optional[np.ndarray] = None
+        self._y: Optional[np.ndarray] = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray) -> KNN:
+        self._X = X
+        self._y = y
+        return self
+
+    def transform(self, X: np.ndarray) -> np.ndarray:
+        closest = []
+        predicted = np.zeros(X.shape[0])
+
+        for j in range(X.shape[0]):
+            for i in range(self._X.shape[0]):
+                distance = np.linalg.norm(X[j] - self._X[i])
+                if len(closest) < self.k:
+                    closest.append((distance, self._y[i]))
+                elif distance < max(closest, key=lambda x: x[0]):
+                    closest = sorted(closest)[:-1]
+                    closest.append((distance, self._y[i]))
+
+            predicted[j] = Counter(map(lambda x: x[0], closest)).most_common(1)[0][0]
+        return predicted
+
+    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        self.fit(X, y)
+        return self.transform(X)
+
+
+class RustKNN(KNN):
+    def __init__(self, k: int = 3):
+        self.k = k
+
+    def fit_transform(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        return knn_algorithm(X, y)
